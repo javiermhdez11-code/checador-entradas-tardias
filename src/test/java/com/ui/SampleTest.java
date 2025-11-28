3dfaefcf217fa490ca7f7b1e42eeaa16569813cf
javiermhdez11-code <javiermhdez11@gmail.com>
2025-11-27 20:02:45 -0600

    test: Comprobación de fallo de CI con aserción incorrecta

--- src/test/java/com/ui/SampleTest.java
+++ src/test/java/com/ui/SampleTest.java
@@ -1,9 +1,11 @@
+package com.ui;
+
 import static org.testng.Assert.assertEquals;
 
 public class SampleTest {
     @org.testng.annotations.Test
     void sample() {
         // La prueba pasa si el valor esperado (5) coincide con el valor real (5)
-        assertEquals(5, 3 + 2);
+        assertEquals(1, 3 + 2);
     }
 }

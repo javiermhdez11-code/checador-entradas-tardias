efc31a49cf0b401a556b55f59f5c81f706cebdb1
javiermhdez11-code <javiermhdez11@gmail.com>
2025-12-01 19:02:39 -0600

    Descripcion de los cambios en master antes de cambiar a develo

--- src/test/java/com/ui/SampleTest.java
+++ src/test/java/com/ui/SampleTest.java
@@ -1,12 +1,10 @@
 package com.ui;
-
 import static org.testng.Assert.assertEquals;
 import org.testng.annotations.Test;
 
 public class SampleTest {
-
     @Test
     void sample() {
-        assertEquals(5, 3 + 2);
+        assertEquals(10, 3 + 2);
     }
 }
